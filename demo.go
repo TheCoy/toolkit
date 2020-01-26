16068c66844ec4e0961eea112424822c6ac68364
The Coy <coy20150428@gmail.com>
2020-01-26 13:33:38 +0800

    remove main func

--- demo.go
+++ demo.go
@@ -6,12 +6,6 @@ package main
 import "fmt"
 import "math/rand"
 import "time"
-import "rsc.io/quote"
-
-func main() {
-	fmt.Println("===golib main===")
-	fmt.Println(quote.Hello())
-}
 
 // SayHello It is a say hi demo
 func SayHello(lan string) {
