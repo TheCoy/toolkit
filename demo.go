109a44aa11262cf4f92703986f5c8675eb9ad069
The Coy <coy20150428@gmail.com>
2020-02-07 14:28:01 +0800

    add stringReverse

--- demo.go
+++ demo.go
@@ -47,3 +47,12 @@ func SwitchDemo(k int) {
 		fmt.Println("default case")
 	}
 }
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	n, h := len(runes), len(runes)/2
+	for i := 0; i < h; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+	return string(runes)
+}
