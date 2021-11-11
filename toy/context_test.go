55d4d7cfc07f5a50fabfcc40177f0635c8df3b5a
The Coy <coy20150428@gmail.com>
2021-11-11 10:16:06 +0800

    fix: unit test

--- toy/context_test.go
+++ toy/context_test.go
@@ -7,20 +7,24 @@ import (
     "time"
 )
 
-func TestContext(t *testing.T) {
+func TestCancelChannelTest(t *testing.T) {
     cancelChannelTest()
+}
 
-    //cancelContextTest
+func TestCancelContextTest(t *testing.T) {
     wg := new(sync.WaitGroup)
     ctx, cancel := context.WithCancel(context.Background())
     wg.Add(1)
-    go cancelContextTest(ctx, wg)
-    time.Sleep(1 * time.Second)
-    cancel()
-    wg.Wait()
 
-    //cancelContextTestV2
-    ctx2, cancel2 := context.WithTimeout(context.Background(), 1 * time.Second)
+    go cancelContextTest(ctx, wg)
+    time.Sleep(3 *time.Second)
+    cancel()
+
+    wg.Wait()
+}
+
+func TestCancelContextTestV2(t *testing.T) {
+    ctx2, cancel2 := context.WithTimeout(context.Background(), 3 * time.Second)
     defer cancel2()
     cancelContextTestV2(ctx2)
 }
